074173ef4324baf33c4fafe91d60077657890e2c
Gabriel <43631819+gpietrafesavieitez@users.noreply.github.com>
2018-10-18 19:32:22 +0200

    Update Restaurante.java

--- Boletin_4_4/src/boletin_4_4/Restaurante.java
+++ Boletin_4_4/src/boletin_4_4/Restaurante.java
@@ -3,16 +3,35 @@ package boletin_4_4;
 public class Restaurante {
     private int clientes, polbo, patacas;
     
-    public Restaurante(){
+    public Restaurante() {
         clientes = polbo = patacas = 0;
-       
+        
     }
     
-    public int clienteStock(int polbo, int patacas){
-        int total;
-        total = ((polbo + patacas) * 3) / 3;
-
-        return total;
+    public int clienteStock(int polbo, int patacas) {
+        this.polbo += polbo;
+        this.patacas += patacas;
+        return ((this.polbo + this.patacas) * 3) / 3;
+        
+    }
+    
+    public void engadirPolbo(int x) {
+        this.polbo += x;      
+        
+    }
+    
+    public void engadirPatacas(int x) {
+        this.patacas += x;
+        
+    }
+    
+    public void amosarPolbo() {
+        System.out.println("- " + polbo + " kg de polbo.");
+        
+    }
+    
+    public void amosarPatacas() {
+        System.out.println("- " + patacas + " kg de patacas.");
         
     }
     

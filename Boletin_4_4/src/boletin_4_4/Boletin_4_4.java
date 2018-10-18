96729801c4d5611a78eaf198d2f25fa194e8ed67
Gabriel <43631819+gpietrafesavieitez@users.noreply.github.com>
2018-10-18 19:32:44 +0200

    Update Boletin_4_4.java

--- Boletin_4_4/src/boletin_4_4/Boletin_4_4.java
+++ Boletin_4_4/src/boletin_4_4/Boletin_4_4.java
@@ -4,8 +4,15 @@ public class Boletin_4_4 {
 
     public static void main(String[] args) {
         Restaurante obxRestaurante = new Restaurante();
-        System.out.println(obxRestaurante.clienteStock(2, 1));
-
+        obxRestaurante.amosarPolbo();
+        obxRestaurante.amosarPatacas();
+        obxRestaurante.engadirPolbo(2);
+        obxRestaurante.engadirPatacas(1);
+        obxRestaurante.amosarPolbo();
+        obxRestaurante.amosarPatacas();
+        System.out.println("- Clientes máximos para el stock actual: " + obxRestaurante.clienteStock(0, 0));
+        System.out.println("- Clientes máximos para el stock actual: " + obxRestaurante.clienteStock(2, 1));
+        
     }
     
 }
